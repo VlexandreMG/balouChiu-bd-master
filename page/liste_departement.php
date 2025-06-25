06df2bdaf09615a26e43494111acb3df8c951717
ChiuTien <chiumandresychristain@gmail.com>
2025-06-25 10:18:09 +0300

    Changement bootstrap

--- page/liste_departement.php
+++ page/liste_departement.php
@@ -9,9 +9,8 @@ $dept = getDepartement();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <title>Liste dept</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
@@ -51,8 +50,5 @@ $dept = getDepartement();
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
